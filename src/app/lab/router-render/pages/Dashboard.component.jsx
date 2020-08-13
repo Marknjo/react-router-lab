854b2753f56bf7116778fb167bbf1be7db63796a
Mark Njoroge <marknjo7@gmail.com>
2020-08-13 10:58:21 +0300

    Refactored Loading component utility

--- src/app/lab/router-render/pages/Dashboard.component.jsx
+++ src/app/lab/router-render/pages/Dashboard.component.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
+import Loading from "../../../helpers/Loading.component";
 
 const Dashboard = ({ fetchDashboard }) => {
-	const content = "Loading";
-
 	const [name, setName] = useState("null");
 	const [message, setMessage] = useState("null");
 	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(true);
-	const [loadingText, setLoadingText] = useState(content);
 
 	useEffect(() => {
 		const unSub = fetchDashboard()
@@ -27,36 +25,12 @@ const Dashboard = ({ fetchDashboard }) => {
 		return () => window.clearTimeout(unSub);
 	}, [fetchDashboard]);
 
-	useEffect(() => {
-		if (loading) {
-			const id = setInterval(() => {
-				setLoadingText((text) => {
-					console.log(text);
-					console.log(content);
-
-					if (text === `${content}...`) {
-						return content;
-					} else {
-						return `${text}.`;
-					}
-					//return content === `${name}...` ? name : `${content}.`;
-				});
-			}, 300);
-
-			return () => clearInterval(id);
-		}
-	}, [content, loading]);
-
 	if (error) {
 		return <p>{error}</p>;
 	}
 
 	if (loading) {
-		return (
-			<div className="loading">
-				<p className="loading-title">{loadingText}</p>
-			</div>
-		);
+		return <Loading content="Load Dashboard" />;
 	}
 
 	return (
