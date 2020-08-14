6a98a8129d1cd4930609201147cfc7bea485a9dd
Mark Njoroge <marknjo7@gmail.com>
2020-08-14 06:55:44 +0300

    First Commit

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "normalize.css";
+import "./styles/App.css";
+
+const App = () => (
+	<div className="container">
+		<h1>React Hooks Lab</h1>
+	</div>
+);
+
+export default App;
