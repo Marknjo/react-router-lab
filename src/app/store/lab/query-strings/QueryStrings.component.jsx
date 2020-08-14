c736fff671a24b5ccb97c62806a48e5ba585e371
Mark Njoroge <marknjo7@gmail.com>
2020-08-14 10:49:10 +0300

    Experimented with query strings.
    Skills gained.
    - Use of query-string npm package. Specificaly use of queryString.parse() and queryString.stringfy()
    - Use of useLocation
      - pathname - is the base path
      - search - a string of query parameters. Empty when there are not.
    
    - use of useRefs
      - I noted that useRefs does not allow bindings with effects. I can say it's a quick and dirty way of binding input values but not at real time as binding using useState and onChange effect.
    
    - Noted that queryString.parse() causes side effects. Handle it inside the useEffects and listen for when the search method of useLocation changes.
    
    - I did some cool interested tricks in this module.
      - hide things depending on circumstace.
      - handle empty submission error
      - prevented submitting empty query strings
      - handle 404 pages
    - I have used four components
      - search page -> BlogSearchFilter
      - Home page -> Home
      - Router => QueryStringRoute
      - 404 page => Page404
    - Remove inline css
    - Combine those query params to one set (useState) object that accepts an object rather handling it separately
    - Refactor the components to cleaner separate compositions.
    - Any other after thought for cleaner code is acceptable (:

---
+++ src/app/store/lab/query-strings/QueryStrings.component.jsx
@@ -0,0 +1,289 @@
+import React, { useRef, useState, useEffect } from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	useHistory,
+	Link,
+	useLocation,
+} from "react-router-dom";
+import queryString from "query-string";
+
+/**
+ * Query String Route - handles the routing
+ * A serach component to display search query
+ * A simple form
+ *  - category
+ *  - keyword
+ *  - phrase
+ *
+ */
+
+const BlogSearchFilter = () => {
+	const labelStyles = {
+		padding: "0.2rem",
+		marginTop: "0.5rem",
+		display: "flex",
+		flexFlow: "nowrap column",
+	};
+	const labelNameStyles = {
+		color: "#306279",
+		display: "inline-block",
+		fontWeight: "bold",
+		fontSize: "0.8rem",
+		marginBottom: "0.25rem",
+	};
+	const displayContainerStyles = {
+		display: "flex",
+		flexFlow: "nowrap column",
+		marginTop: "1.5rem",
+		padding: "1.5rem",
+		boxShadow: "0 2px 15px 5px rgba(0,0,0, 0.25)",
+		minWidth: "22rem",
+	};
+	const displayElementLabelStyles = {
+		color: "#cacaca",
+		fontWeight: "bold",
+		textShadow: "0 2px 1px rgba(0, 0, 0, 0.25)",
+		display: "inline-block",
+		padding: "0 1rem",
+	};
+
+	const history = useHistory();
+	const { search } = useLocation();
+
+	const [searchStatus, setSearchStatus] = useState(false);
+
+	const phraseRef = useRef("");
+	const keywordRef = useRef("");
+	const categoryRef = useRef("");
+
+	const [queryPhrase, setQueryPhrase] = useState(null);
+	const [queryKeyword, setQueryKeyword] = useState(null);
+	const [queryCategory, setQueryCategory] = useState(null);
+	const [error, setError] = useState(null);
+
+	const handleSerchSubmit = () => {
+		const phrase = phraseRef.current.value;
+		const keyword = keywordRef.current.value;
+		const category = categoryRef.current.value;
+
+		let query = {};
+
+		if (phrase) query = { ...query, phrase };
+		if (keyword) query = { ...query, keyword };
+		if (category) query = { ...query, category };
+
+		const string = queryString.stringify(query);
+
+		if (phrase || keyword || category) {
+			history.push(`/search/?${string}`);
+			//clear the values
+			if (phrase) query = phraseRef.current.value = "";
+			if (keyword) query = keywordRef.current.value = "";
+			if (category) query = categoryRef.current.value = "";
+
+			//split the query parameters and set them
+			setSearchStatus(true);
+			//set search to true
+		} else {
+			setError(
+				"Empty search parameters not allowed.  Kindly ensure to submit your search with at least one value filled."
+			);
+		}
+	};
+
+	useEffect(() => {
+		const url = queryString.parse(search);
+		url.phrase && setQueryPhrase(url.phrase);
+		url.keyword && setQueryKeyword(url.keyword);
+		url.category && setQueryCategory(url.category);
+	}, [search]);
+
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				flexFlow: "nowrap column",
+			}}
+		>
+			<div style={displayContainerStyles}>
+				{error ? (
+					<>
+						<h2> Error </h2>
+						<p>{error}</p>
+						<button
+							style={{
+								padding: "0.5rem",
+								marginTop: "1rem",
+							}}
+							onClick={() => setError(null)}
+						>
+							Go back to search
+						</button>
+						<br />
+						or
+						<Link to="/">Go to Home Page</Link>
+					</>
+				) : (
+					<>
+						<h1
+							style={{
+								color: "#306279",
+								paddingBottom: "0.5rem",
+								borderBottom: "1px solid #ccc",
+								textShadow: "0 2px 1px rgba(0, 0, 0, 0.25)",
+								marginBottom: "0.5rem",
+							}}
+						>
+							Search Parameters
+						</h1>
+						<label htmlFor="earch-phrase" style={labelStyles}>
+							<span style={labelNameStyles}>Search Phrase</span>
+							<input
+								type="text"
+								name="phrase"
+								id="search-phrase"
+								ref={phraseRef}
+							/>
+						</label>
+						<label htmlFor="search-keyword" style={labelStyles}>
+							<span style={labelNameStyles}>Search keyword</span>
+							<input
+								type="text"
+								name="keyword"
+								id="search-keyword"
+								ref={keywordRef}
+							/>
+						</label>
+						<label htmlFor="search-category" style={labelStyles}>
+							<span style={labelNameStyles}>Search Category</span>
+							<input
+								type="text"
+								name="category"
+								id="search-category"
+								ref={categoryRef}
+							/>
+						</label>
+						<button
+							style={{
+								padding: "0.5rem",
+								marginTop: "1rem",
+							}}
+							onClick={handleSerchSubmit}
+						>
+							Search
+						</button>
+
+						{search && (
+							<button
+								style={{
+									padding: "0.5rem",
+									marginTop: "1rem",
+								}}
+								onClick={() => {
+									setSearchStatus(false);
+									setQueryPhrase(null);
+									setQueryKeyword(null);
+									setQueryCategory(null);
+									history.push("/search");
+								}}
+							>
+								Clear Search Queries
+							</button>
+						)}
+					</>
+				)}
+			</div>
+
+			{searchStatus && (
+				<div
+					style={{
+						...displayContainerStyles,
+						backgroundColor: "#306279",
+						marginTop: "2.5rem",
+						color: "#ccc",
+					}}
+				>
+					<h2
+						style={{
+							borderBottom: "4px solid #285265",
+							paddingBottom: "0.83rem",
+							textShadow: "0 2px 1px rgba(0, 0, 0, 0.25)",
+						}}
+					>
+						Query Search Parameters
+					</h2>
+					<p>
+						<span style={displayElementLabelStyles}>Query: </span>
+						<code>{search}</code>
+					</p>
+					{queryPhrase && (
+						<p>
+							<span style={displayElementLabelStyles}>Phrase: </span>
+							{queryPhrase}
+						</p>
+					)}
+					{queryKeyword && (
+						<p>
+							<span style={displayElementLabelStyles}>Keyword: </span>
+							{queryKeyword}
+						</p>
+					)}
+					{queryCategory && (
+						<p>
+							<span style={displayElementLabelStyles}>Category: </span>
+							{queryCategory}
+						</p>
+					)}
+
+					<Link to="/">Go to Home Page</Link>
+				</div>
+			)}
+		</div>
+	);
+};
+
+const Page404 = () => (
+	<div
+		style={{
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+			flexFlow: "nowrap column",
+		}}
+	>
+		<h1>404 Page Not Found</h1>
+		<Link to="/">Go to Home Page</Link>
+	</div>
+);
+
+const Home = () => (
+	<div
+		style={{
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+			flexFlow: "nowrap column",
+		}}
+	>
+		<h1>Home Page</h1>
+		<Link to="/search">Visit Blog</Link>
+	</div>
+);
+
+const QueryStringRoute = () => {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route path="/search" component={BlogSearchFilter} />
+				<Route path="*" component={Page404} />
+			</Switch>
+		</Router>
+	);
+};
+
+export default QueryStringRoute;
