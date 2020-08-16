16621a573a0547918ea10ce1635bd6ce228e987c
Mark Njoroge <marknjo7@gmail.com>
2020-08-16 14:44:38 +0300

    Added Nav Links

--- src/app/lab/sidebar/Sidebar.component.jsx
+++ src/app/lab/sidebar/Sidebar.component.jsx
@@ -3,7 +3,8 @@ import {
   BrowserRouter as Router, 
   Link, 
   Switch, 
-  Route 
+  Route, 
+  NavLink
 } from 'react-router-dom';
 
 
@@ -30,52 +31,65 @@ const routes = [
 		main: () => <h2>Bubblegum</h2>,
 	},
 	{
-		path: "/shoe-laces",
+		path: "/shoelaces",
 		sidebar: () => (
 			<>
-				<h1>Shoe Laces Page </h1>
+				<h1>Shoelaces Page </h1>
 			</>
 		),
-		main: () => <h2>Shoe Laces</h2>,
+		main: () => <h2>Shoelaces</h2>,
 	},
 ];
 
+const Navs = () => {
+  return (
+    <nav className="s-nav-main">
+      <ul className="s-nav">
+        <li className="s-nav__item"><NavLink className="s-nav--link" to="/">Home</NavLink></li>
+        <li className="s-nav__item"><NavLink className="s-nav--link" to="/bubblegum">Bubblegum</NavLink></li>
+        <li className="s-nav__item"><NavLink className="s-nav--link" to="/shoelaces"></NavLink>Shoelaces</li>
+      </ul>
+    </nav>
+  )
+}
+
 export const Sidebar = () => {
   return (
-		<div className="main-content">
-			<aside className="sidebar">
-				<div className="sidebar-header">
-					<div className="sidebar-header__title">
-						<p>Sidebar</p>
+		<Router>
+			<div className="main-content">
+				<aside className="sidebar">
+					<div className="sidebar-header">
+						<div className="sidebar-header__title">
+							<p>Sidebar</p>
+						</div>
+						<div className="sidebar-header__content">
+							<p>Content</p>
+						</div>
 					</div>
-					<div className="sidebar-header__content">
+					<div className="sidebar-content">
+						<Navs />
+					</div>
+				</aside>
+				<div className="content-container">
+					<header className="content-container__header">
+						<p>Sidebar Header</p>
+					</header>
+					<section className="content-container__main">
+						<p>Content</p>
+					</section>
+				</div>
+				<footer className="footer">
+					<div className="footer-content">
 						<p>Content</p>
 					</div>
-				</div>
-				<div className="sidebar-content">
-					<p>Sidebar Links</p>
-				</div>
-			</aside>
-			<div className="content-container">
-				<header className="content-container__header">
-					<p>Sidebar Header</p>
-				</header>
-				<section className="content-container__main">
-					<p>Content</p>
-				</section>
+					<div className="footer-copyright-info">
+						<p>
+							<span>Footer Area</span> <small>Copyright</small>
+						</p>
+					</div>
+				</footer>
 			</div>
-			<footer className="footer">
-				<div className="footer-content">
-					<p>Content</p>
-				</div>
-				<div className="footer-copyright-info">
-					<p>
-						<span>Footer Area</span> {" "}
-						<small>Copyright</small>
-					</p>
-				</div>
-			</footer>
-		</div>
+		</Router>
 	);
 }
 
