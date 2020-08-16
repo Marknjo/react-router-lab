2c946aa5521f09e34d445560fe030675a478738b
Mark Njoroge <marknjo7@gmail.com>
2020-08-16 11:16:35 +0300

    Refactored the timer component to a separate file :)

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "normalize.css";
 import "./styles/App.css";
-import BaseRouter from "./lab/timer-app/Timer.component";
+import BaseRouter from "./lab/timer-app/BaseRouter.component";
 
 const App = () => (
 	<div className="container">
